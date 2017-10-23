e38206cfbef73f3e0c886519d82b24112b85d2de
Miftah Mizwar <miftahmizwar@gmail.com>
2017-10-23 17:01:55 +0700

    initial commit, simple quiz app

--- SimpleQuiz/ViewController.swift
+++ SimpleQuiz/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var UIQuestionLabel: UILabel!
+    
+    @IBOutlet weak var UIAnswerLabel: UILabel!
+    
+    var indexOfQuestion: Int = 0
+    
+    let questions: [String] = [
+        "Who am i? I'm yellow and love laughing",
+        "What is Capital of Jakarta?",
+        "Where am i?"
+    ]
+    
+    let answers: [String] = [
+        "Spongebob S.",
+        "J",
+        "beside am"
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        UIQuestionLabel.text = questions[indexOfQuestion]
+        UIAnswerLabel.text = "?"
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func NextButtonClicked(_ sender: UIButton) {
+        indexOfQuestion = indexOfQuestion + 1
+        if (indexOfQuestion == questions.count){
+            indexOfQuestion = 0
+        }
+        
+        let question = questions[indexOfQuestion]
+        UIQuestionLabel.text = question
+        UIAnswerLabel.text = "?"
+        
+    }
+    
+    @IBAction func ShowButtonClicked(_ sender: Any) {
+        UIAnswerLabel.text = answers[indexOfQuestion]
+    }
+    
 }
 
